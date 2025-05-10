702bd25c9b519f27ce9aeb142e12e4d8f5af97df
Al <111322015+oaui@users.noreply.github.com>
2025-05-10 19:18:03 +0000

    Added card styling

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -3,9 +3,26 @@ import React, { useState } from "react";
 function Card(props) {
 
     /* */
-    return (<div>
-        <h2>Casdfasdfasdfsdfasdfsard</h2>
-    </div>)
+    return (
+        <div>
+            <div className="card">
+                <div className="cardHeader">
+                    Title
+                </div>
+                <div className="cardContent">
+                    <div className="imageContainer">
+
+                    </div>
+                    <div className="cardInfo">
+                        <p>Info text bla</p>
+                    </div>
+                </div>
+                <div className="cardFooter">
+                    <p>Footer content bla</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
